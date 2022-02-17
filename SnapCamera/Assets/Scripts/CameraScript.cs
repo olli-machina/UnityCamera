52ef227ba7706c13ac70211c8f14334b14842b3c
Olli Machina <olli.machina@mymail.champlain.edu>
2022-02-17 11:15:39 -0500

    Added moving subject for photos

--- SnapCamera/Assets/Scripts/CameraScript.cs
+++ SnapCamera/Assets/Scripts/CameraScript.cs
@@ -6,7 +6,8 @@ public class Camera : MonoBehaviour
 {
     public Camera photoCamera;
     public RenderTexture photoTexture;
-
+    public Vector2 moveBounds;
+    public float speed;
 
     // Start is called before the first frame update
     void Start()
@@ -17,5 +18,23 @@ public class Camera : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void checkMouse()
+    {
+        if (Input.mousePosition.x > Screen.width - moveBounds.x)
+        {
+            //move camera
+        }
+    }
+
+    void takePhoto()
+    { 
+        Texture2D tex;
+        tex = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
+        tex.ReadPixels(new Rect(0,0,Screen.width, Screen.height), 0, 0, false);
+        tex.Apply();
+        GetComponent<Renderer>().material.mainTexture = tex;
+
     }
 }
